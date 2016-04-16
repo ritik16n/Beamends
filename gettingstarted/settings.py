072b1ca347fd3d025450b1d084bf3b296adcf95e
ritik <saxenar444@gmail.com>
2016-04-16 12:06:07 +0530

    project restore error solved

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -156,7 +156,7 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 #STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_ROOT=os.path/join(BASE_DIR,'hello','static')
+STATIC_ROOT=os.path.join(BASE_DIR,'hello','static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
