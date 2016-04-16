1a6b0c4e8078f7d48a2bf3a5c67805c072c7b19f
ritik <saxenar444@gmail.com>
2016-04-16 12:09:09 +0530

    further renovations

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'
+    'django.contrib.sites',
+    'hello',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
